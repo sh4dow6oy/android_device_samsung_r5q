31cb0fb1f2e18a295b61eb8c1ae77b9e193dd27b
sh4dow6oy <109531621+sh4dow6oy@users.noreply.github.com>
2026-03-27 13:59:46 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_r3q-user
-add_lunch_combo omni_r3q-userdebug
-add_lunch_combo omni_r3q-eng
+add_lunch_combo omni_r5q-user
+add_lunch_combo omni_r5q-userdebug
+add_lunch_combo omni_r5q-eng
