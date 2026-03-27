47ac512af9582698a953ae95ba6155a9f0afc204
sh4dow6oy <109531621+sh4dow6oy@users.noreply.github.com>
2026-03-27 13:04:39 +0200

    Update and rename omni_r3q.mk to twrp_r5q.mk

--- twrp_r5q.mk
+++ twrp_r5q.mk
@@ -12,18 +12,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit from r3q device
-$(call inherit-product, device/samsung/r3q/device.mk)
+# Inherit from r5q device
+$(call inherit-product, device/samsung/r5q/device.mk)
 
-PRODUCT_DEVICE := r3q
-PRODUCT_NAME := omni_r3q
+PRODUCT_DEVICE := r5q
+PRODUCT_NAME := twrp_r3q
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-A908B
+PRODUCT_MODEL := SM-G770F
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="r3qxx-user 11 RP1A.200720.012 A908BXXU5EVK3 release-keys"
+    PRIVATE_BUILD_DESC="r5qxx-user 11 RP1A.200720.012 A908BXXU5EVK3 release-keys"
 
-BUILD_FINGERPRINT := samsung/r3qxx/r3q:11/RP1A.200720.012/A908BXXU5EVK3:user/release-keys
+BUILD_FINGERPRINT := samsung/r5qxx/r5q:11/RP1A.200720.012/G770FXXU5EVK3:user/release-keys
