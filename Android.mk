119f458bf297cdb4d91b90f3fb64367502a6eb27
sh4dow6oy <109531621+sh4dow6oy@users.noreply.github.com>
2026-04-01 15:56:08 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -7,6 +7,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),r5q)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+ifneq ($(filter r5q, $(TARGET_DEVICE)),)
+include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
