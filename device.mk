a9723a07e1d9d802252896386ebc7f6c8d22da0f
sh4dow6oy <109531621+sh4dow6oy@users.noreply.github.com>
2026-04-02 11:56:03 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,47 @@
 #
 
 LOCAL_PATH := device/samsung/r5q
+
+# Arhitectură și nucleu
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Identificare Produs (Folosim variabile standard pentru a evita duplicatele)
+PRODUCT_MODEL := SM-G770F
+PRODUCT_DEVICE := r5q
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_NAME := twrp_r5q
+
+# Versiune Software (Sincronizat cu eroarea ta: Android 12L / SDK 32)
+PRODUCT_PLATFORM_RELEASE := 12
+PLATFORM_SDK_VERSION := 32
+PLATFORM_VERSION := 12
+
+# Proprietăți de Sistem (Overrides)
+# Aici punem doar ce este strict necesar pentru TWRP
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.adb.secure=0 \
+    ro.debuggable=1 \
+    persist.sys.usb.config=adb \
+    ro.secure=0 \
+    ro.allow.mock.location=1 \
+    ro.build.characteristics=default \
+    ro.display.series="Samsung Galaxy S10 Lite"
+
+# Setări specifice Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Partitii și Sistem de fișiere
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    checkpoint_gc \
+    libandroid_net \
+    libion
+
+# Limbă și regiune
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Boot Animatie (dacă există)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml
