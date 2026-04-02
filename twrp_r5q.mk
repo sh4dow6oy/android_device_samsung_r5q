86c9e284cf6a148fea139a2e933da7cbe21c9582
sh4dow6oy <109531621+sh4dow6oy@users.noreply.github.com>
2026-04-02 11:56:47 +0300

    Update twrp_r5q.mk

--- twrp_r5q.mk
+++ twrp_r5q.mk
@@ -14,16 +14,30 @@
 # limitations under the License.
 #
 
-# Inherit from r5q device
+# Inherit from r5q device (Configurarea de bază a device-ului)
 $(call inherit-product, device/samsung/r5q/device.mk)
 
-# Inherit from our custom product configuration
+# Inherit from our custom product configuration (Configurarea TWRP)
 $(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from AOSP base (Asigură-te că aceasta este ultima includere de bază)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-## Device identifier. This must come after all inclusions
+## Device identifier.
+# Trebuie să folosim valori identice peste tot pentru a evita "duplicate sysprop"
 PRODUCT_DEVICE := r5q
 PRODUCT_NAME := twrp_r5q
-PRODUCT_BRAND := Samsung
-PRODUCT_MODEL := Samsung Galaxy S10 Lite
-PRODUCT_MANUFACTURER := Samsung
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-G770F
+PRODUCT_MANUFACTURER := samsung
+
+# Setări specifice de Build pentru a forța versiunea corectă
+PRODUCT_PLATFORM_RELEASE := 12
+PLATFORM_VERSION := 12
+PLATFORM_SDK_VERSION := 32
+
+# Fingerprint oficial (opțional, dar ajută la consistență)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=twrp_r5q \
+    PRODUCT_DEVICE=r5q \
+    TARGET_DEVICE=r5q
